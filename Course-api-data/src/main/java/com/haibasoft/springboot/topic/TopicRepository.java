74490e1803602bd3c42408a16b0615e0c4708bf2
dilanlongla <44490676+dilanlongla@users.noreply.github.com>
2020-12-02 08:51:14 +0100

    Full project on api development with spring boot

---
+++ Course-api-data/src/main/java/com/haibasoft/springboot/topic/TopicRepository.java
@@ -0,0 +1,8 @@
+package com.haibasoft.springboot.topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic, String> {
+
+	
+}
---
+++ Course-api-data/src/test/java/com/haibasoft/springboot/CourseApiDataApplicationTests.java
@@ -0,0 +1,12 @@
+package com.haibasoft.springboot;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApiDataApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
